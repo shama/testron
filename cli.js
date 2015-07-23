f5b82553ab95266ffb7034c9fcb35dbc166cb3fc
Kyle Robinson Young <kyle@dontkry.com>
2015-07-23 15:13:12 -0700

    Ability to show errors via the cli

--- cli.js
+++ cli.js
@@ -2,6 +2,12 @@
 
 process.title = 'testron'
 var path = require('path')
+var argv = require('minimist')(process.argv.slice(2))
+
 var testron = require('./index.js')
-testron(path.resolve(process.cwd(), process.argv[2]))
-  .stdout.pipe(process.stdout)
+var tests = testron(path.resolve(process.cwd(), process.argv[2]))
+tests.stdout.pipe(process.stdout)
+
+if (argv['errors'] || argv['e']) {
+  tests.stderr.pipe(process.stderr)
+}
